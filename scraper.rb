fb802dad182ac9f8d66c73e2fea6e0076915cf28
Matthew Landauer <mlandauer@gmail.com>
2019-05-28 15:01:36 +1000

    Now scrape pages using the library

--- scraper.rb
+++ scraper.rb
@@ -17,20 +17,6 @@ enquiry_search_page = scraper.click_date_search_tab(enquiry_lists_page)
 # The Date tab defaults to a search range of the last 30 days.
 results_page = scraper.click_search_on_page(enquiry_search_page)
 
-count = 0
-while results_page
-  count += 1
-  puts "Parsing the results on page #{count}."
-
-  scraper.scrape_index_page(results_page) do |record|
-    EpathwayScraper.save(record)
-  end
-
-  next_page_image = results_page.root.at_xpath("//td/input[contains(@src, 'nextPage')]")
-  results_page = nil
-  if next_page_image
-    next_page_path = next_page_image['onclick'].split(',').find { |e| e =~ /.*PageNumber=\d+.*/ }.gsub('"', '').strip
-    puts "Retrieving the next page: #{next_page_path}"
-    results_page = agent.get "#{base_url}/GeneralEnquiry/#{next_page_path}"
-  end
+scraper.scrape_all_index_pages_with_gets(nil) do |record|
+  EpathwayScraper.save(record)
 end
